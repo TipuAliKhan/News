64170d6709e10dba8ed1a742a48bb6c7d71aa05a
Tipu <tipualikhan@gmail.com>
2021-04-12 10:25:03 +0530

    added active class to header component

--- news/src/Components/Header/Header.js
+++ news/src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = ({categories, handleClick}) => {
     return (
         <header>
             <ul>
-                {categories.map((item, index) => <li key={index} onClick={handleClick} title="click to filter news" data-type={item.type} data-value={item.value}>{item.value}</li>)}
+                {categories.map((item, index) => <li key={index} className={item.isActive?'active':null} onClick={handleClick} title="click to filter news" data-type={item.type} data-value={item.value}>{item.value}</li>)}
             </ul>
         </header>
     )
